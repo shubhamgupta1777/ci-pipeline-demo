db613d53c26c0414ba7c20d1dc7f7c6adbb31644
shubhamgupta1777 <shubham.may99@gmail.com>
2025-12-12 14:45:55 +0530

    initial commit

---
+++ src/main/java/com/pipeline/cicd/CiPipelineDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pipeline.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiPipelineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiPipelineDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pipeline/cicd/CiPipelineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipeline.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiPipelineDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
