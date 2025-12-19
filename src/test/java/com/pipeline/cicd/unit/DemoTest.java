5bccecdba172798b84db0ed93fb229f892c767e8
shubhamgupta1777 <shubham.may99@gmail.com>
2025-12-19 10:34:39 +0530

    added a new test method

--- src/test/java/com/pipeline/cicd/unit/DemoTest.java
+++ src/test/java/com/pipeline/cicd/unit/DemoTest.java
@@ -2,17 +2,19 @@ package com.pipeline.cicd.unit;
 
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DemoTest {
 
     @Test
     void checkEven_success(){
-        assertTrue(10%2==0);
+        assertTrue(10%2 == 0);
     }
 
-//    @Test
-//    void checkEven_fail(){
-//        assertTrue(15%2==0);
-//    }
+    @Test
+    void checkEven_fail(){
+        assertFalse(15%2 == 0);
+    }
 }
